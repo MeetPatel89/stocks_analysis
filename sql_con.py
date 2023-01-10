71ebdf38f867173486d9b00b6f652e95c9fcecc7
Meet Patel <meetaiims@yahoo.com>
2023-01-10 00:35:51 -0500

    add def conn_odbc() to get pyodbc connection object

--- sql_con.py
+++ sql_con.py
@@ -1,16 +1,14 @@
 from config import SQL_DRIVER, SQL_SERVER, SQL_DB, SQL_USER, SQL_PWD
+import pyodbc
 
-def conn_string(driver=SQL_DRIVER, server=SQL_SERVER, db=SQL_DB, user=SQL_USER, pwd=SQL_PWD):
-    conn_string = f"""
-        Driver={driver};
-        Server={server};
-        Database={db};
-        Authentication=SqlPassword;
-        Encrypt=yes;
-        TrustServerCertificate=Yes;
-        UID={user};
-        PWD={pwd}
-    """
+def conn_string(driver, server, db, user, pwd):
+    return f"Driver={driver};Server={server};Database={db};Authentication=SqlPassword;Encrypt=yes;TrustServerCertificate=Yes;UID={user};PWD={pwd}"
 
-    return conn_string
+def conn_odbc(driver=SQL_DRIVER, server=SQL_SERVER, db=SQL_DB, user=SQL_USER, pwd=SQL_PWD):
+    try:
+        conn = pyodbc.connect(conn_string(driver, server, db, user, pwd))
+    except pyodbc.Error as e:
+        print("Connection Error:")
+        print(e.args[1])
 
+conn_odbc()
